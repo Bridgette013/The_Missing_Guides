031cdbc7319ac2cfcfba5bb81800740f88a32b31
Bridgette013 <195794981+Bridgette013@users.noreply.github.com>
2026-04-25 03:08:05 -0700

    edit to price id pointing to netlify env vars

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     } else {
       const priceMap = {
         // TEMPORARY: Test mode price ID - switch back to live price after testing
-        'alcohol': 'price_1SwlW2BgiaEKVIS7jniM4JLG',
+        'alcohol': process.env.STRIPE_PRICE_ALCOHOL,
         'opioid': process.env.STRIPE_PRICE_OPIOID,
         'stimulant': process.env.STRIPE_PRICE_STIMULANT,
         'benzo': process.env.STRIPE_PRICE_BENZO,
